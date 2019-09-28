395127f83c6d1a4681f1231c3e8398803a8c9d42
JavaScript Joe <jjprevite@gmail.com>
2019-09-27 20:24:43 -0700

    feat: add exercises from chp 2

---
+++ chapter-2/src/index.ts
@@ -0,0 +1,20 @@
+console.log('Hello TypeScript!')
+
+let a = 1 + 1
+let b = a + 3
+let c = {
+  apple: a,
+  banana: b
+}
+let d = c.apple + 4
+
+let dogs = ['4', 7, 5, 'hello']
+
+const myDog = {
+  name: 'Kona',
+  age: '4 months'
+}
+
+const names = ['Kona', 'Rosie']
+
+names[0] = 'Joe'
