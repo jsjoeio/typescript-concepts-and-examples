f08a897a4329415750a0ec0285b8664836130947
Joe Previte <jjprevite@gmail.com>
2019-10-10 15:46:47 -0700

    feat: add chess example

---
+++ examples/src/chess.ts
@@ -0,0 +1,52 @@
+// An exercise from Chapter 5 on "Classes and Interfaces" in _Programming TypeScript_
+
+// Represents a game of chess
+
+type Color = "Black" | "White";
+// In ches,, they use standard algebraic notation
+// the x-axis are called "Files"
+// the y-axis are called "Ranks"
+// Normally called File, but conflicts with File in browser
+// So we opted for ChessFile
+type ChessFile = "A" | "B" | "C" | "D" | "E" | "F" | "G" | "H";
+type Rank = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8;
+
+class Position {
+  constructor(private file: ChessFile, private rank: Rank) {}
+  distanceFrom(position: Position) {
+    return {
+      rank: Math.abs(position.rank - this.rank),
+      file: Math.abs(position.file.charCodeAt(0) - this.file.charCodeAt(0))
+    };
+  }
+}
+
+abstract class Piece {
+  protected position: Position;
+  constructor(private readonly color: Color, file: ChessFile, rank: Rank) {
+    this.position = new Position(file, rank);
+  }
+  moveTo(position: Position) {
+    this.position = position;
+  }
+  abstract canMoveTo(position: Position): boolean;
+}
+
+// There are 6 types of pieces
+class King extends Piece {
+  canMoveTo(position: Position) {
+    let distance = this.position.distanceFrom(position);
+    return distance.rank < 2 && distance.file < 2;
+  }
+}
+
+class Game {
+  private static makePieces() {
+    return [
+      // Kings
+      new King("White", "E", 1),
+      new King("Black", "E", 8)
+    ];
+  }
+  private pieces = Game.makePieces();
+}
